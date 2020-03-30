262177bb5074aa8f49574ba8357a0445a65d57e5
Xcalizorz <zadjad.rezai@outlook.com>
2020-03-31 00:16:28 +0200

    Implements hostname lookup in API

--- learn_docker_app/api/respond.py
+++ learn_docker_app/api/respond.py
@@ -1,3 +1,6 @@
+import platform
+
+from flask import request
 from flask_restx import fields, Namespace, Resource
 
 respond_namespace = Namespace(
@@ -6,14 +9,46 @@ respond_namespace = Namespace(
 )
 
 copy_cat_model = respond_namespace.model('Respond', {
-        'response': fields.String(required=True, description='The string provided.')
-    }
-)
+        'response': fields.String(description='The string provided.')
+})
 
 @respond_namespace.route('/<string>')
-class SimpleResponse(Resource):
+class Copycat(Resource):
     @respond_namespace.marshal_with(copy_cat_model)
     def get(self, string: str):
         return {
             'response': string,
+        }
+
+
+
+platform_model = respond_namespace.model('Host', {
+    'hostname_or_ip': fields.String(
+        description='The computer’s network name (may not be fully qualified!) or its ip address.' \
+                    'An empty string is returned if the value cannot be determined.'
+    ),
+    'system': fields.String(
+        description='Returns the system/OS name, such as "Linux", "Darwin", ' \
+                    '"Java", "Windows". An empty string is returned if the' \
+                    'value cannot be determined.'
+    )
+})
+hostname_model = respond_namespace.model('Hostname', {
+    'host': fields.Nested(platform_model, description='Data concerning the API server.'),
+    'client': fields.Nested(platform_model, description='Data concerning the requesting client.'),
+})
+
+@respond_namespace.route('/hostname')
+class Hostname(Resource):
+    @respond_namespace.marshal_with(hostname_model)
+    def get(self):
+        return {
+            'host': {
+                'hostname_or_ip': platform.node(),
+                'system': platform.system(),
+            },
+            'client': {
+                'hostname_or_ip': request.remote_addr,
+                'system': request.user_agent.platform,
+            },
         }
