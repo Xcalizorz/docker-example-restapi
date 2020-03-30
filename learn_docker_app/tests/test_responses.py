e8894b99d341770580ea7586cf21217ce155bb13
Xcalizorz <zadjad.rezai@outlook.com>
2020-03-30 22:54:31 +0200

    Adds test for Hostname API

--- learn_docker_app/tests/test_responses.py
+++ learn_docker_app/tests/test_responses.py
@@ -13,11 +13,14 @@ def client():
 
 
 class TestResponse:
+    def copy_cat_response(self, test_string: str):
+        return f"/respond/{test_string}"
+
     @pytest.mark.parametrize("test_string", [
         "hi", "bye", "xxx", "123!", "-12312",
     ])
     def test_simple_response__simple_message(self, client, test_string):
-        response = client.get(f"/respond/{test_string}")
+        response = client.get(self.copy_cat_response(test_string))
 
         assert response.status_code == 200
         assert response.json == {'response': test_string}
@@ -26,6 +29,15 @@ class TestResponse:
         "../", "\\//..", "cd ..//",
     ])
     def test_simple_response__wrong_url(self, client, test_string):
-        response = client.get(f"/respond/{test_string}")
+        response = client.get(self.copy_cat_response(test_string))
 
         assert response.status_code == 404
+
+    def test_server_hostname(self, client):
+        response = client.get(f"/respond/hostname")
+
+        assert response.status_code == 200
+        assert response.json == {
+            'hostname': 'TEST-HOSTNAME',
+            'requester': 'TEST-REQUESTER',
+        }
